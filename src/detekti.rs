8a85d958ef8894caca0d89a094930dfab7c8f4e0
Jak Belo <3452896431@qq.com>
2026-06-15 21:07:48 +0800

    Ripari la problemon de lingvoŝanĝo inter malsamaj fenestroj.

--- src/detekti.rs
+++ src/detekti.rs
@@ -1,9 +1,18 @@
-use windows::Win32::UI::Input::KeyboardAndMouse::GetKeyboardLayout;
+use windows::Win32::UI::{
+    Input::KeyboardAndMouse::GetKeyboardLayout,
+    WindowsAndMessaging::{GetForegroundWindow, GetWindowThreadProcessId},
+};
 
 pub fn estas_esperanta() -> bool {
     unsafe {
+        // Akiri la antaŭan fenestron.
+        let fg_fenestro = GetForegroundWindow();
+
+        // Akiri la fadenan identigilon de la antaŭa fenestro.
+        let fadena_id = GetWindowThreadProcessId(fg_fenestro, None);
+
         // Klavara pritraktilo.
-        let kp = GetKeyboardLayout(0);
+        let kp = GetKeyboardLayout(fadena_id);
         let lin_id = kp.0 as isize;
         lin_id == 67706880 // Esperanta lingvo-identigilo.
     }
