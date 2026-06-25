ad7c1de71afc89d83d75b494150d220b653a8a49
Jak Belo <3452896431@qq.com>
2026-06-25 19:08:33 +0800

    Kaŝi la konzolon en eldono.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 use windows::Win32::UI::WindowsAndMessaging::{
     DispatchMessageW, GetMessageW, HHOOK, MSG, SetWindowsHookExW, TranslateMessage, UnhookWindowsHookEx, WH_KEYBOARD_LL
 };
